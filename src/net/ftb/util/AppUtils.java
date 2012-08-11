8b64a3abc41a3cd1093b1fb1313b8bb2d890de3b
Andrew Okin <forkk@forkk.net>
2012-08-11 12:26:43 -0500

    Added downloadString() method.

--- src/net/ftb/util/AppUtils.java
+++ src/net/ftb/util/AppUtils.java
@@ -1,7 +1,11 @@
 package net.ftb.util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class AppUtils
 {
@@ -10,11 +14,30 @@ public class AppUtils
 	 * @param throwable the throwable to get a stack trace from.
 	 * @return a string representing the given throwable's stack trace.
 	 */
-	public String getStackTrace(Throwable throwable)
+	public static String getStackTrace(Throwable throwable)
 	{
 		StringWriter sWriter = new StringWriter();
 		PrintWriter pWriter = new PrintWriter(sWriter);
 		throwable.printStackTrace(pWriter);
 		return sWriter.toString();
+	}
+	
+	/**
+	 * Downloads data from the given URL and returns it as a string.
+	 * @param url the URL to fetch data from.
+	 * @return the data downloaded from the given URL as a string.
+	 * @throws IOException if url.openStream throws an IOException
+	 */
+	public static String downloadString(URL url) throws IOException
+	{
+		InputStream dlStream = url.openStream();
+		StringWriter strWriter = new StringWriter();
+		int c;
+		while ((c = dlStream.read()) != -1)
+		{
+			strWriter.write(c);
+		}
+		
+		return strWriter.toString();
 	}
 }
