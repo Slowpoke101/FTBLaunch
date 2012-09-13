3a77bf7587969625081d2e9b2d7ea1c4397e697a
Vbitz <jscarsbrook@gmail.com>
2012-09-13 17:00:00 +1200

    Console Works

--- src/net/ftb/gui/LauncherConsole.java
+++ src/net/ftb/gui/LauncherConsole.java
@@ -2,9 +2,12 @@ package net.ftb.gui;
 
 import javax.swing.JDialog;
 import javax.swing.JTextArea;
+import javax.swing.SwingWorker;
 
 import java.awt.Dimension;
 import java.awt.Toolkit;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
@@ -12,7 +15,35 @@ import java.io.PrintStream;
 public class LauncherConsole extends JDialog {
 	private static final long serialVersionUID = 1L;
 	
-	private JTextArea textArea;
+	final JTextArea textArea;
+	
+	private class OutputOverride extends PrintStream {
+		
+		public OutputOverride(OutputStream str) throws FileNotFoundException {
+			super(str);
+		}
+		
+		@Override
+		public void write(byte[] b) throws IOException {
+			textArea.append(new String(b));
+			// write it to the console
+			super.write(b);
+		}
+		
+		@Override
+		public void write(byte[] buf, int off, int len) {
+			textArea.append(new String(buf, off, len));
+			// write it to the console
+			super.write(buf, off, len);
+		}
+		
+		@Override
+		public void write(int b) {
+			// write it to the console
+			super.write(b);
+		}
+		
+	}
 	
 	public LauncherConsole() throws IOException {
 		setTitle("FTB Launcher Console");
@@ -25,5 +56,6 @@ public class LauncherConsole extends JDialog {
 		textArea.setBounds(10, 11, 425, 250);
 		getContentPane().add(textArea);
 		textArea.setColumns(10);
+		System.setOut(new OutputOverride(System.out));
 	}
 }
