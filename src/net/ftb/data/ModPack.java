95538e2794f990b0aae6b2c872c656bad398a67a
jjw123 <sr.jason123@yahoo.co.uk>
2012-09-21 17:14:12 +0100

    added size variable to ModPack

--- src/net/ftb/data/ModPack.java
+++ src/net/ftb/data/ModPack.java
@@ -1,18 +1,25 @@
 package net.ftb.data;
 
 import java.awt.Image;
+import java.io.IOException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class ModPack {
 	
 	private String name;
 	private URL url;
+	private URLConnection connection;
 	private Image image;
+	private int size;
 	
-	public ModPack(String name, URL url, Image image) {
+	
+	public ModPack(String name, URL url, Image image) throws IOException {
 		this.name = name;
 		this.url = url;
 		this.image = image;
+		connection = url.openConnection();
+		this.size = connection.getContentLength();
 	}
 	
 	public String getName() {
@@ -25,5 +32,9 @@ public class ModPack {
 	
 	public Image getImage() {
 		return image;
+	}
+	
+	public int getSize() {
+		return size;
 	}
 }
