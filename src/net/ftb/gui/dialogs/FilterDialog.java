aa0ee0a150faa79638948d33e3c2c9a5dbc7fefb
Sean Schwartz <unvcustoms@gmail.com>
2012-11-06 09:07:16 -0600

    Start work on Filter Dialog

--- src/net/ftb/gui/dialogs/FilterDialog.java
+++ src/net/ftb/gui/dialogs/FilterDialog.java
@@ -2,13 +2,17 @@ package net.ftb.gui.dialogs;
 
 import java.awt.Toolkit;
 
+import javax.swing.JComboBox;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import net.ftb.gui.LaunchFrame;
 
 public class FilterDialog extends JDialog {
 	private JPanel panel = new JPanel();
+	private JLabel typeLbl = new JLabel("Mod Pack Type:"), originLbl = new JLabel("Mod Pack Origin:");
+	private JComboBox typeBox = new JComboBox(new String[] {"Client", "Server"}), originBox = new JComboBox(new String[] {"All", "FTB", "3rd Party"});
 	
 	public FilterDialog(LaunchFrame instance) {
 		super(instance, true);
@@ -18,8 +22,19 @@ public class FilterDialog extends JDialog {
 		setBounds(300, 300, 350, 230);
 		setResizable(false);
 		
+		panel.setBounds(0, 0, 350, 230);
+		panel.setLayout(null);
 		setContentPane(panel);
+                
+		typeLbl.setBounds(10, 10, 100, 30);
+		typeBox.setBounds(120, 10, 100, 30);
 		
-		
+		originLbl.setBounds(10, 40, 100, 30);
+		originBox.setBounds(120, 40, 100, 30);
+                
+		panel.add(typeLbl);
+		panel.add(typeBox);
+		panel.add(originLbl);
+		panel.add(originBox);
 	}
 }
