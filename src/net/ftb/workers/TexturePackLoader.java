8a2405279ebc0b0a4ff4ffb8b557729a4b232be7
unv_annihilator <unvcustoms@gmail.com>
2012-11-09 16:49:03 -0600

    Finish TexturePackLoader

--- src/net/ftb/workers/TexturePackLoader.java
+++ src/net/ftb/workers/TexturePackLoader.java
@@ -1,5 +1,53 @@
 package net.ftb.workers;
 
-public class TexturePackLoader {
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.NoSuchAlgorithmException;
 
+import net.ftb.data.TexturePack;
+import net.ftb.gui.panes.TexturepackPane;
+import net.ftb.log.Logger;
+import net.ftb.util.AppUtils;
+
+import org.w3c.dom.DOMException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class TexturePackLoader extends Thread {
+	private static String TEXTUREPACKFILE;
+	
+	public TexturePackLoader() { }
+	
+	@Override
+	public void run() {
+		try {
+			Logger.logInfo("loading texture pack information...");
+			
+//			TEXTUREPACKFILE = LaunchFrame.getCreeperhostLink("texturepack.xml");
+			TEXTUREPACKFILE = "https://dl.dropbox.com/u/2405919/texturepack.xml";
+			
+			Document doc = AppUtils.downloadXML(new URL(TEXTUREPACKFILE));
+			if(doc == null) {
+				Logger.logError("Error: Could not load texture pack data!");
+			}
+			NodeList texturePacks = doc.getElementsByTagName("texturepack");
+			for(int i = 0; i < texturePacks.getLength(); i++) {
+				Node texturePack = texturePacks.item(i);
+				NamedNodeMap mapAttr = texturePack.getAttributes();
+				TexturePack.addTexturePack(new TexturePack(mapAttr.getNamedItem("name").getTextContent(), mapAttr.getNamedItem("author").getTextContent(),
+						mapAttr.getNamedItem("version").getTextContent(), mapAttr.getNamedItem("url").getTextContent(),
+						mapAttr.getNamedItem("logo").getTextContent(), mapAttr.getNamedItem("image").getTextContent(),
+						mapAttr.getNamedItem("mcversion").getTextContent(), i));
+			}
+			TexturepackPane.loaded = true;
+		} catch (MalformedURLException e) { 
+		} catch (IOException e) { 
+		} catch (SAXException e) { 
+		} catch (NoSuchAlgorithmException e) { 
+		} catch (DOMException e) { }
+	}
 }
