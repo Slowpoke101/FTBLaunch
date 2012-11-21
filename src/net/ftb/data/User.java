81602c29ad4067e011714f149d2d237b0601023f
jjw123 <sr.jason123@yahoo.co.uk>
2012-11-21 00:01:15 +0000

    more comment (not project, comment)
    excuse the typo in the last commit :3

--- src/net/ftb/data/User.java
+++ src/net/ftb/data/User.java
@@ -3,12 +3,20 @@ package net.ftb.data;
 public class User {
 	private String _username = "", _password = "", _name = "";
 
+	/**
+	 * @param username - the username of the profile
+	 * @param password - the password of the profile
+	 * @param name - the name of the profile
+	 */
 	public User(String username, String password, String name) {
 		_username = username;
 		_password = password;
 		_name = name;
 	}
 
+	/**
+	 * @param input - text with username, password, name
+	 */
 	public User(String input) {
 		String[] tokens = input.split(":");
 		if(tokens.length == 3) {
@@ -21,30 +29,51 @@ public class User {
 		}
 	}
 
+	/**
+	 * @return - profile username
+	 */
 	public String getUsername() {
 		return _username;
 	}
 
+	/**
+	 * @param username - set profile username
+	 */
 	public void setUsername(String username) {
 		_username = username;
 	}
 
+	/**
+	 * @return - profile password
+	 */
 	public String getPassword() {
 		return _password;
 	}
 
+	/**
+	 * @param password - set profile password
+	 */
 	public void setPassword(String password) {
 		_password = password;
 	}
 
+	/**
+	 * @return - profile name
+	 */
 	public String getName() {
 		return _name;
 	}
 
+	/**
+	 * @param name - set profile name
+	 */
 	public void setName(String name) {
 		_name = name;
 	}
 
+	/**
+	 * @return - a string with username, password, name in
+	 */
 	@Override
 	public String toString() {
 		return _name + ":" + _username + ":" + _password; 
