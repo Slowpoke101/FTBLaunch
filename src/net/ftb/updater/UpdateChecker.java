3cea069a1cb5154b22ec17d1f42af0087d098b76
unv_annihilator <unvcustoms@gmail.com>
2012-11-26 10:42:46 -0600

    Don't grab creeperhost link unless needed.

--- src/net/ftb/updater/UpdateChecker.java
+++ src/net/ftb/updater/UpdateChecker.java
@@ -21,7 +21,7 @@ public class UpdateChecker {
 	private int version;
 	private int latest;
 	public static String verString = "";
-	private URL downloadUrl;
+	private String downloadAddress = "";
 
 	public UpdateChecker(int version) {
 		this.version = version;
@@ -46,17 +46,10 @@ public class UpdateChecker {
 				verString += temp[i] + ".";
 			}
 			verString += temp[temp.length - 1];
-			String downloadAddress = updateAttributes.getNamedItem("downloadURL").getTextContent();
-			if (downloadAddress.indexOf("http") != 0) {
-				// TODO: Make this link work, aka upload the newest launcher onto creeperhost. 
-				// Will be named FTB_Launcher.exe or FTB_Launcher.jar
-				downloadAddress = LaunchFrame.getCreeperhostLink(downloadAddress);
-			}
-			downloadUrl = new URL(downloadAddress);
+			downloadAddress = updateAttributes.getNamedItem("downloadURL").getTextContent();
 		} catch (MalformedURLException e) { 
 		} catch (IOException e) { 
-		} catch (SAXException e) { 
-		} catch (NoSuchAlgorithmException e) { }
+		} catch (SAXException e) { }
 	}
 
 	public boolean shouldUpdate() {
@@ -72,14 +65,14 @@ public class UpdateChecker {
 		String temporaryUpdatePath = OSUtils.getDynamicStorageLocation() + File.separator + "updatetemp" + File.separator + path.substring(path.lastIndexOf(File.separator) + 1);
 		String extension = path.substring(path.lastIndexOf('.') + 1);
 		extension = "exe".equalsIgnoreCase(extension) ? extension : "jar";
-
 		try {
-			URL updateURL = new URL(downloadUrl.toString() + "." + extension);
+			URL updateURL = new URL(LaunchFrame.getCreeperhostLink(downloadAddress + "." + extension));
 			File temporaryUpdate = new File(temporaryUpdatePath);
 			temporaryUpdate.getParentFile().mkdir();
 			FileUtils.downloadToFile(updateURL, temporaryUpdate);
 			SelfUpdate.runUpdate(path, temporaryUpdatePath);
 		} catch (MalformedURLException e) { Logger.logError("Malformed download URL for launcher update", e);
-		} catch (IOException e) { Logger.logError("Failed to download launcher update", e); }
+		} catch (IOException e) { Logger.logError("Failed to download launcher update", e); 
+		} catch (NoSuchAlgorithmException e) { }
 	}
 }
