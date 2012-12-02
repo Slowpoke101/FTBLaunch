9a6ff5ad4a8c7b504821548bda675c1cc3948523
jjw123 <sr.jason123@yahoo.co.uk>
2012-12-02 15:37:22 +0000

    update version numbers to 1.1.2

--- src/net/ftb/gui/LaunchFrame.java
+++ src/net/ftb/gui/LaunchFrame.java
@@ -78,8 +78,8 @@ import net.ftb.workers.LoginWorker;
 
 public class LaunchFrame extends JFrame {
 
-	private static String version = "1.1.1";
-	private static int buildNumber = 111;
+	private static String version = "1.1.2";
+	private static int buildNumber = 112;
 	public static final String FORGENAME = "MinecraftForge.zip";
 	private NewsPane newsPane;
 	private OptionsPane optionsPane;
