d8ad38c67141c816a6df55f53542043ec12935fa
unv_annihilator <unvcustoms@gmail.com>
2012-12-26 01:04:33 -0600

    Fix issue with modpacks being marked as unloaded.
    Issue was due to a single xml being unable to load thus preventing all
    remaining xmls to load and the packs not being marked as loaded.
    Fix spacing on error/warn output.

--- src/net/ftb/workers/ModpackLoader.java
+++ src/net/ftb/workers/ModpackLoader.java
@@ -19,24 +19,21 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class ModpackLoader extends Thread {
-	
+
 	private String[] xmlFile;
-	
+
 	public ModpackLoader(String[] xmlFile) {
 		this.xmlFile = xmlFile;
 	}
-	
+
 	@Override
 	public void run() {
-		
 		for(int j = 0; j < xmlFile.length; j++) {
 			boolean privatePack = false;
-			
 			if(!xmlFile[j].equalsIgnoreCase("modpacks.xml")) {
 				privatePack = true;
 			}
-			
-			File modPackFile = new File(OSUtils.getDynamicStorageLocation() + File.separator + "ModPacks" + File.separator + xmlFile[j]);
+			File modPackFile = new File(OSUtils.getDynamicStorageLocation(), "ModPacks" + File.separator + xmlFile[j]);
 			try {
 				modPackFile.getParentFile().mkdirs();
 				DownloadUtils.downloadToFile(new URL(DownloadUtils.getStaticCreeperhostLink(xmlFile[j])), modPackFile);
@@ -49,41 +46,42 @@ public class ModpackLoader extends Thread {
 			try {
 				modPackStream = new FileInputStream(modPackFile);
 			} catch(IOException e) {
-				Logger.logWarn("Failed to read modpackfile - falling back to direct download", e);
+				Logger.logWarn("Failed to read modpack file - falling back to direct download", e);
 			}
 			if(modPackStream == null) {
 				try {
 					modPackStream = new URL(DownloadUtils.getStaticCreeperhostLink(xmlFile[j])).openStream();
 				} catch(IOException e) {
-					Logger.logError("Completely unable to download the modpackfile - check your connection", e);
-					return;
+					Logger.logError("Completely unable to download the modpack file - check your connection", e);
 				}
 			}
-			try {
-				doc = AppUtils.getXML(modPackStream);
-			} catch (Exception e) {
-				Logger.logError("Exception reading modpackfile", e);
-				return;
-			}
-			if(doc == null) {
-				Logger.logError("Error: could not load modpackdata!");
-				return;
-			}
-			NodeList modPacks = doc.getElementsByTagName("modpack");
-			for(int i = 0; i < modPacks.getLength(); i++) {
-				Node modPackNode = modPacks.item(i);
-				NamedNodeMap modPackAttr = modPackNode.getAttributes();
+			if(modPackStream != null) {
 				try {
-					ModPack.addPack(new ModPack(modPackAttr.getNamedItem("name").getTextContent(), modPackAttr.getNamedItem("author").getTextContent(),
-							modPackAttr.getNamedItem("version").getTextContent(), modPackAttr.getNamedItem("logo").getTextContent(),
-							modPackAttr.getNamedItem("url").getTextContent(), modPackAttr.getNamedItem("image").getTextContent(),
-							modPackAttr.getNamedItem("dir").getTextContent(), modPackAttr.getNamedItem("mcVersion").getTextContent(), 
-							modPackAttr.getNamedItem("serverPack").getTextContent(), modPackAttr.getNamedItem("description").getTextContent(),
-							modPackAttr.getNamedItem("mods") != null ? modPackAttr.getNamedItem("mods").getTextContent() : "", 
-							modPackAttr.getNamedItem("oldVersions") != null ? modPackAttr.getNamedItem("oldVersions").getTextContent() : "",
-							modPackAttr.getNamedItem("animation") != null ? modPackAttr.getNamedItem("animation").getTextContent() : "", i, privatePack));
+					doc = AppUtils.getXML(modPackStream);
 				} catch (Exception e) {
-					Logger.logError(e.getMessage(), e);
+					Logger.logError("Exception reading modpack file", e);
+					return;
+				}
+				if(doc == null) {
+					Logger.logError("Error: could not load modpack data!");
+					return;
+				}
+				NodeList modPacks = doc.getElementsByTagName("modpack");
+				for(int i = 0; i < modPacks.getLength(); i++) {
+					Node modPackNode = modPacks.item(i);
+					NamedNodeMap modPackAttr = modPackNode.getAttributes();
+					try {
+						ModPack.addPack(new ModPack(modPackAttr.getNamedItem("name").getTextContent(), modPackAttr.getNamedItem("author").getTextContent(),
+								modPackAttr.getNamedItem("version").getTextContent(), modPackAttr.getNamedItem("logo").getTextContent(),
+								modPackAttr.getNamedItem("url").getTextContent(), modPackAttr.getNamedItem("image").getTextContent(),
+								modPackAttr.getNamedItem("dir").getTextContent(), modPackAttr.getNamedItem("mcVersion").getTextContent(), 
+								modPackAttr.getNamedItem("serverPack").getTextContent(), modPackAttr.getNamedItem("description").getTextContent(),
+								modPackAttr.getNamedItem("mods") != null ? modPackAttr.getNamedItem("mods").getTextContent() : "", 
+										modPackAttr.getNamedItem("oldVersions") != null ? modPackAttr.getNamedItem("oldVersions").getTextContent() : "",
+												modPackAttr.getNamedItem("animation") != null ? modPackAttr.getNamedItem("animation").getTextContent() : "", i, privatePack));
+					} catch (Exception e) {
+						Logger.logError(e.getMessage(), e);
+					}
 				}
 			}
 		}
