e684101a406d7b9007d637731a9a44d9c2e57502
unv_annihilator <unvcustoms@gmail.com>
2013-01-09 19:51:37 -0600

    Fix version box not being set correctly.

--- src/net/ftb/gui/panes/ModpacksPane.java
+++ src/net/ftb/gui/panes/ModpacksPane.java
@@ -3,16 +3,13 @@ package net.ftb.gui.panes;
 import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Dimension;
-import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.io.File;
-import java.net.MalformedURLException;
 import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 import javax.swing.ImageIcon;
@@ -64,7 +61,7 @@ public class ModpacksPane extends JPanel implements ILauncherPane, ModPackListen
 	private static HashMap<Integer, ModPack> currentPacks = new HashMap<Integer, ModPack>();
 	private final ModpacksPane instance = this;
 	private static JEditorPane packInfo;
-	
+
 
 	//	private JLabel loadingImage;
 	public static String origin = "All", mcVersion = "All", avaliability = "All";
@@ -180,10 +177,10 @@ public class ModpacksPane extends JPanel implements ILauncherPane, ModPackListen
 		infoScroll.setViewportView(packInfo);
 		infoScroll.setOpaque(false);
 		add(infoScroll);
-		
+
 		server = new JButton("Download Server");
 		server.setBounds(420, 5, 130, 25);
-		
+
 		server.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent event) {
@@ -205,17 +202,18 @@ public class ModpacksPane extends JPanel implements ILauncherPane, ModPackListen
 			}
 		});
 		add(server);
-		
+
 		version = new JComboBox<String>(new String[]{});
 		version.setBounds(560, 5, 130, 25);
 		version.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				Settings.getSettings().setPackVer(String.valueOf(version.getSelectedItem()));
+				Settings.getSettings().setPackVer((String.valueOf(version.getSelectedItem()).equalsIgnoreCase("recommended") ? "Recommended Version" : String.valueOf(version.getSelectedItem())));
+				Settings.getSettings().save();
 			}
 		});
 		add(version);
-		
+
 		privatePack = new JButton("Private Packs");
 		privatePack.setBounds(700, 5, 120, 25);
 		privatePack.addActionListener(new ActionListener() {
@@ -314,38 +312,35 @@ public class ModpacksPane extends JPanel implements ILauncherPane, ModPackListen
 	private static void updatePacks() {
 		for(int i = 0; i < packPanels.size(); i++) {
 			if(selectedPack == i) {
-				String mods = "";
-				if(ModPack.getPack(getIndex()).getMods() != null) {
-					mods += "<p>This pack contains the following mods by default:</p><ul>";
-					for (String name : ModPack.getPack(getIndex()).getMods()) {
-						mods += "<li>" + name + "</li>";
+				ModPack pack = ModPack.getPack(getIndex());
+				if(pack != null) {
+					String mods = "";
+					if(pack.getMods() != null) {
+						mods += "<p>This pack contains the following mods by default:</p><ul>";
+						for (String name : ModPack.getPack(getIndex()).getMods()) {
+							mods += "<li>" + name + "</li>";
+						}
+						mods += "</ul>";
 					}
-					mods += "</ul>";
-				}
-				packPanels.get(i).setBackground(UIManager.getColor("control").darker().darker());
-				packPanels.get(i).setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
-				File tempDir = new File(OSUtils.getDynamicStorageLocation(), "ModPacks" + File.separator + ModPack.getPack(getIndex()).getDir());
-				packInfo.setText("<html><img src=' file:\\"+ tempDir.getPath() + File.separator + ModPack.getPack(getIndex()).getImageName() +"' width=400 height=200></img> <br>" + ModPack.getPack(getIndex()).getInfo() + mods);
-				packInfo.setCaretPosition(0);
-				
-				if(ModPack.getSelectedPack().getServerUrl().equals("") || ModPack.getSelectedPack().getServerUrl() == null) {
-					server.setEnabled(false);
-				} else {
-					server.setEnabled(true);
-				}
-				
-				if(ModPack.getPack(getIndex()) != null) {
+					packPanels.get(i).setBackground(UIManager.getColor("control").darker().darker());
+					packPanels.get(i).setCursor(Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR));
+					File tempDir = new File(OSUtils.getDynamicStorageLocation(), "ModPacks" + File.separator + ModPack.getPack(getIndex()).getDir());
+					packInfo.setText("<html><img src=' file:\\"+ tempDir.getPath() + File.separator + ModPack.getPack(getIndex()).getImageName() +"' width=400 height=200></img> <br>" + ModPack.getPack(getIndex()).getInfo() + mods);
+					packInfo.setCaretPosition(0);
+
+					if(ModPack.getSelectedPack().getServerUrl().equals("") || ModPack.getSelectedPack().getServerUrl() == null) {
+						server.setEnabled(false);
+					} else {
+						server.setEnabled(true);
+					}
+					String tempVer = Settings.getSettings().getPackVer();
 					version.removeAllItems();
 					version.addItem("Recommended");
-					if(ModPack.getPack(getIndex()).getOldVersions() != null) {
-						for(String s : ModPack.getPack(getIndex()).getOldVersions()) {
-							version.addItem(s);
-						}
-					}
+					for(String s : pack.getOldVersions()) {
+						version.addItem(s);
+					});
+					version.setSelectedItem(tempVer);
 				}
-				
-				version.setSelectedItem(Settings.getSettings().getPackVer());
-				
 			} else {
 				packPanels.get(i).setBackground(UIManager.getColor("control"));
 				packPanels.get(i).setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
