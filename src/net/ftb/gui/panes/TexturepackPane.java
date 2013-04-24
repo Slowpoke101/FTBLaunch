9e3a9367d5897f3071c0aebb3f4bdfdfe738ff27
unv_annihilator <unvcustoms@gmail.com>
2013-04-24 16:42:42 -0500

    Fixed improper character, updated pom.xml

--- src/net/ftb/gui/panes/TexturepackPane.java
+++ src/net/ftb/gui/panes/TexturepackPane.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of FTB Launcher.
  *
- * Copyright © 2012-2013, FTB Launcher Contributors <https://github.com/Slowpoke101/FTBLaunch/>
+ * Copyright Â© 2012-2013, FTB Launcher Contributors <https://github.com/Slowpoke101/FTBLaunch/>
  * FTB Launcher is licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
