810cc5d4f6654f508b880cd52d1a9abbc02a748a
Janne Kujanpää <jikuja@iki.fi>
2014-05-26 23:52:18 +0300

    CHANGED: LogType.java: use comparable interface

--- src/net/ftb/log/LogType.java
+++ src/net/ftb/log/LogType.java
@@ -19,24 +19,11 @@ package net.ftb.log;
 public enum LogType {
     DEBUG, EXTENDED, MINIMAL;
 
-    private static Integer currentPrecedence;
-    private int precedence = currentPrecedence();
-
     public boolean includes (LogType other) {
-        return other.precedence >= this.precedence;
+        return other.compareTo(this) >= 0;
     }
 
     public String toString () {
         return name().substring(0, 1) + name().substring(1).toLowerCase();
-    }
-
-    /**
-     * Workaround for limitations on usage of static variables in enum field initialisers.
-     */
-    private int currentPrecedence () {
-        if (currentPrecedence == null) {
-            currentPrecedence = 0;
-        }
-        return currentPrecedence++;
     }
 }
