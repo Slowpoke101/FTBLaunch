ef5bde99649262e286490aa208348c484f651054
progwml6 <progwml6@gmail.com>
2014-08-16 15:45:57 -0400

    start moving the layout to use miglayout in modpack filter, edit/add profile dialogs

---
+++ src/main/java/net/ftb/gui/GuiConstants.java
@@ -0,0 +1,10 @@
+package net.ftb.gui;
+
+public class GuiConstants {
+public static final String WRAP = "wrap";
+public static final String CENTER = "center";
+public static final String CENTER_SINGLE_LINE = "center, wrap, span";
+public static final String CENTER_TWO =  "split 2, span 2, center";
+
+    public static final String SEP = ", ";
+}
