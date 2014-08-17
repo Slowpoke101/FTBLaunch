2db6500d577fb3998382464bc0fa448e179838c8
progwml6 <progwml6@gmail.com>
2014-08-16 20:12:47 -0400

    convert advanced options to miglayout

--- src/main/java/net/ftb/gui/GuiConstants.java
+++ src/main/java/net/ftb/gui/GuiConstants.java
@@ -7,6 +7,8 @@ package net.ftb.gui;
         public static final String CENTER_TWO =  "split 2, span 2, center";
         public static final String FILL_TWO =  "split 2, span 2, grow";
         public static final String FILL_THREE =  "split 3, span 3, grow";
+        public static final String SPLIT_4 = "split 4, span 4";
+        public static final String FILL_FOUR = SPLIT_4 + ", grow";
         public static final String FILL_SINGLE_LINE = "wrap, span, grow";
         public static final String SEP = ", ";
         public static final String GROW = "grow";
