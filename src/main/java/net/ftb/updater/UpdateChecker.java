8948d8dcd2137bdd41d8aee0f4674055d240a9fd
Progwml6 <progwml6@gmail.com>
2014-10-26 20:34:18 -0400

    change update file name

--- src/main/java/net/ftb/updater/UpdateChecker.java
+++ src/main/java/net/ftb/updater/UpdateChecker.java
@@ -88,7 +88,7 @@ public class UpdateChecker extends SwingWorker<Boolean, Void> {
 
     private void loadInfo () {
         try {
-            Document doc = AppUtils.downloadXML(new URL(DownloadUtils.getStaticCreeperhostLink("version.xml")));
+            Document doc = AppUtils.downloadXML(new URL(DownloadUtils.getStaticCreeperhostLink("newversion.xml")));
             Update upd = JsonFactory.getUpdate("net.ftb:launcher:beta@json", Locations.FTBMAVENFULL);
             if (upd.getPrimary().equals("beta")) {
                 Channel beta = upd.getBeta();
