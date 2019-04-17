1e6dd6c8166f184f55cfd55391fb256bec6fa83c
yufrice <c1155208@cstu.nit.ac.jp>
2019-04-17 19:56:19 +0900

    Add jaJP to locale

--- src/main/java/net/ftb/locale/I18N.java
+++ src/main/java/net/ftb/locale/I18N.java
@@ -36,7 +36,7 @@ public class I18N {
     public static Locale currentLocale = Locale.enUS;
 
     public enum Locale {
-        cyGB, daDK, deDE, enGB, enUS, esES, fiFI, frCA, frFR, itIT, maHU, nlNL, noNO, plPL, ptBR, ptPT, ruRU, svSE, zhCN, zhTW
+        cyGB, daDK, deDE, enGB, enUS, esES, fiFI, frCA, frFR, jaJP, itIT, maHU, nlNL, noNO, plPL, ptBR, ptPT, ruRU, svSE, zhCN, zhTW
     }
 
     /**
