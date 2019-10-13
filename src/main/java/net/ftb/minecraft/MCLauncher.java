cf5f419591e96694d4632a0a4ceca8c425b0c931
Progwml6 <progwml6@gmail.com>
2019-10-13 16:27:02 -0400

    disable debug

--- src/main/java/net/ftb/minecraft/MCLauncher.java
+++ src/main/java/net/ftb/minecraft/MCLauncher.java
@@ -324,7 +324,7 @@ public class MCLauncher {
         }
 
         ProcessBuilder builder = new ProcessBuilder(arguments);
-
+        /*
         StringBuilder tmp = new StringBuilder();
         for (String a : builder.command()) {
             tmp.append(a).append(" \n");
