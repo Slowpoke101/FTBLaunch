cbebeef15ff43d9605f5f4400e9ec74a788c5196
modmuss50 <modmuss50@gmail.com>
2019-10-29 13:36:37 +0000

    Fix ClassLoading issues on J9 +

---
+++ src/main/java/net/ftb/main/Bootstrap.java
@@ -0,0 +1,37 @@
+/*
+ * This file is part of FTB Launcher.
+ *
+ * Copyright © 2012-2018, FTB Launcher Contributors <https://github.com/Slowpoke101/FTBLaunch/>
+ * FTB Launcher is licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.ftb.main;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * This is required to ensure the loader is loaded via a URLClassLoader, LauncherClassLoader is used only to make addUrl public
+ */
+public class Bootstrap {
+
+    public static final LauncherClassLoader CLASS_LOADER = new LauncherClassLoader(Bootstrap.class.getClassLoader());
+
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Thread.currentThread().setContextClassLoader(CLASS_LOADER);
+
+        Class<?> mainClass = CLASS_LOADER.loadClass("net.ftb.main.Main");
+        Method mainMethod = mainClass.getMethod("main", String[].class);
+        mainMethod.invoke(null, (Object) args);
+    }
+
+}
